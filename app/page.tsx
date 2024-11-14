f9dcceb86eed49b22f8a0a17953e9edc6084c352
ViniciusOliver-stack <vinicius.so.contato@hotmail.com>
2024-11-14 20:59:16 -0300

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,125 @@
+"use client"
+
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { TextGenerateEffectExample } from "@/components/ui/textGenerateEffect"
+import { useToast } from "@/hooks/use-toast"
+import { Overpass_Mono } from "next/font/google"
+import Image from "next/image"
+import Link from "next/link"
+import { useState } from "react"
+
+const overPass = Overpass_Mono({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-overPass",
+  weight: ["300", "400", "500", "600", "700"],
+})
+
+const date = new Date()
+const year = date.getFullYear()
+
+export default function Home() {
+  const [email, setEmail] = useState("")
+  const toast = useToast()
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    try {
+      const response = await fetch(
+        "https://hook.us1.make.com/opry7ec9g82a2pd2eo44vnsx126g8v1h",
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({ email }),
+        }
+      )
+
+      // Se o envio for bem-sucedido, atualiza a mensagem de sucesso
+      if (response.ok) {
+        toast.toast({
+          title: "E-mail enviado com sucesso!",
+          description: "Fique atento ao seu e-mail para mais informações.",
+        })
+      } else {
+        toast.toast({
+          title: "Erro ao enviar o e-mail.",
+          description: "Por favor, tente novamente mais tarde.",
+          variant: "destructive",
+        })
+      }
+    } catch (error) {
+      console.error(error)
+      toast.toast({
+        title: "Erro ao enviar o e-mail.",
+        description: "Por favor, tente novamente mais tarde.",
+        variant: "destructive",
+      })
+    }
+  }
+
+  return (
+    <div className="min-h-screen flex flex-col select-none">
+      <header className="w-[65%] mx-auto mt-8 flex justify-between items-center">
+        <Image src="/logo.svg" alt="logo" width={150} height={150} />
+
+        <Button asChild>
+          <Link href="https://wa.me/557788500766" target="_blank">
+            Fale conosco
+          </Link>
+        </Button>
+      </header>
+      <main className="flex-grow w-[65%] mx-auto mt-8 flex flex-col items-center justify-center">
+        <section className="flex flex-col gap-4 -mt-40 items-center justify-center">
+          <h1
+            className={`text-[60px] leading-[72px] text-center  ${overPass.className}`}
+          >
+            EXPERIÊNCIA DA NOVA GERAÇÃO DE AGENTES DE{" "}
+            <span className="text-blue-500">IA</span> PARA NEGÓCIOS
+          </h1>
+          <div className="flex flex-col gap-1">
+            <p className="text-xl leading-4 text-center mx-auto font-light">
+              <TextGenerateEffectExample
+                text="Aumente suas conversões e melhore o atendimento ao cliente 24/7
+                com agentes de IA"
+                duration={0.3}
+              />
+            </p>
+            <p className="text-xl leading-4 text-center w-[70%] mx-auto font-light">
+              <TextGenerateEffectExample
+                text="Que entendem e interagem como humanos."
+                duration={0.5}
+              />
+            </p>
+          </div>
+
+          <div className="flex flex-col items-center justify-center mt-6">
+            <form
+              onSubmit={handleSubmit}
+              className="flex w-full max-w-sm items-center justify-center"
+            >
+              <Input
+                type="email"
+                placeholder="E-mail"
+                className="focus:outline-none focus:ring-0"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
+              <Button type="submit" className="rounded-l-none">
+                <span className="flex items-center gap-2">
+                  {/* <div className="w-4 h-4 border-2 border-white border-t-transparent rounded-full animate-spin" /> */}
+                  Lista de espera
+                </span>
+              </Button>
+            </form>
+          </div>
+        </section>
+      </main>
+      <footer className="flex items-center justify-center py-4 gap-3">
+        <Image src="/icon.svg" alt="icon" width={30} height={30} />
+        <p className="text-gray-600 text-sm">{`© ${year} Rubnik | Todos os Direitos Reservado`}</p>
+      </footer>
+    </div>
+  )
+}
---
+++ components/ui/textGenerateEffect.tsx
@@ -0,0 +1,82 @@
+// @NOTE: in case you are using Next.js
+"use client"
+
+import { cn } from "@/lib/utils"
+import { motion } from "framer-motion"
+
+type TextGenerateEffectExampleProps = {
+  text: string
+  duration?: number
+}
+
+export function TextGenerateEffectExample({
+  text,
+  duration,
+}: TextGenerateEffectExampleProps) {
+  //   const text = `This is a text generate effect component.`
+  return <TextGenerateEffect text={text} preset="char" duration={duration} />
+}
+
+type TextGenerateEffectProps = {
+  text: string
+  duration?: number
+  preset?: "char" | "word"
+} & React.ComponentProps<"span">
+
+export function TextGenerateEffect({
+  text,
+  duration = 0.3,
+  className,
+  preset = "char",
+}: TextGenerateEffectProps) {
+  return (
+    <motion.div className="inline-block whitespace-pre">
+      {preset === "char"
+        ? text.split("").map((char, index) => (
+            <motion.span
+              key={char + index}
+              className={cn("whitespace-pre inline-block ", className)}
+              initial={{ opacity: 0, filter: "blur(4px)", rotateX: 90, y: 5 }}
+              whileInView={{
+                opacity: 1,
+                filter: "blur(0px)",
+                rotateX: 0,
+                y: 0,
+              }}
+              transition={{
+                ease: "easeOut",
+                duration: duration,
+                delay: index * 0.015,
+              }}
+              viewport={{ once: true }}
+            >
+              {char}
+            </motion.span>
+          ))
+        : text.split(" ").map((word, index) => (
+            <motion.span
+              key={word + index}
+              className={cn(
+                "whitespace-pre inline-block text-neutral-400",
+                className
+              )}
+              initial={{ opacity: 0, filter: "blur(4px)", rotateX: 90, y: 5 }}
+              whileInView={{
+                opacity: 1,
+                filter: "blur(0px)",
+                rotateX: 0,
+                y: 0,
+              }}
+              transition={{
+                ease: "easeOut",
+                duration: duration,
+                delay: index * 0.015,
+              }}
+              viewport={{ once: true }}
+            >
+              {word}{" "}
+            </motion.span>
+          ))}
+    </motion.div>
+  )
+}
